3a83abe6a8cad298a929a4c2fd1ecdfdff326245
CHHUEN CHIHAY <hay.mario168@gmail.com>
2025-05-09 10:56:35 +0700

    MidtermDevOps

---
+++ src/main/java/gic/i4/library_webapp_chhuen_chihay/LibraryWebappChhuenChihayApplication.java
@@ -0,0 +1,13 @@
+package gic.i4.library_webapp_chhuen_chihay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryWebappChhuenChihayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryWebappChhuenChihayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gic/i4/library_webapp_chhuen_chihay/LibraryWebappChhuenChihayApplicationTests.java
@@ -0,0 +1,13 @@
+// package gic.i4.library_webapp_chhuen_chihay;
+
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+// class LibraryWebappChhuenChihayApplicationTests {
+
+// 	@Test
+// 	void contextLoads() {
+// 	}
+
+// }
